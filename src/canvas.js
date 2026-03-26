c9603f769cbf425cec76b480e5209a9135261bb2
Karan Chavan <karanchavan287@gmail.com>
2026-03-27 02:26:39 +0530

    complete

--- src/canvas.js
+++ src/canvas.js
@@ -1,6 +1,3 @@
-// import { randomIntFromRange, randomColor, distance } from './utils/utils.js';
-// import { color3 } from './utils/colorArrays.js';
-
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
@@ -31,14 +28,19 @@ class Circle {
         this.radius = radius;
         this.color = color;
         this.velocity = velocity;
-        this.tTL = 1000;
+        this.tTL = 700;
 
         this.draw = () => {
-            c.beginPath();
-            c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-            c.fillStyle = this.color;
-            c.fill();
-            c.closePath();
+            this.draw = () => {
+                c.save();
+                c.globalAlpha = this.tTL / 700; // Fades out over time
+                c.beginPath();
+                c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+                c.fillStyle = this.color;
+                c.fill();
+                c.closePath();
+                c.restore();
+            }
         }
 
         this.update = () => {
@@ -52,7 +54,7 @@ class Circle {
 }
 
 let particles;
-const particlesCount = 100;
+const particlesCount = 50;
 function init() {
     particles = [];
     const tunnelRadius = 30;
@@ -64,24 +66,26 @@ function init() {
         // const y = canvas.height / 2 +  * tunnelRadius;
         const x = canvas.width / 2;
         const y = canvas.height / 2;
-        particles.push(new Circle(x, y, 5, 'blue', { x: Math.cos(radian * i), y: Math.sin(radian * i) }));
+        particles.push(new Circle(x, y, 5, 'white', { x: Math.cos(radian * i), y: Math.sin(radian * i) }));
     }
 }
 
 function generateRing() {
-    setTimeout(generateRing, 1000);
+    setTimeout(generateRing, 250);
     for (let i = 0; i < particlesCount; i++) {
         const radian = (Math.PI * 2) / particlesCount;
-        const x = canvas.width / 2;
-        const y = canvas.height / 2;
-        particles.push(new Circle(x, y, 5, 'blue', { x: Math.cos(radian * i), y: Math.sin(radian * i) }));
+        const x = mouse.x;
+        const y = mouse.y;
+        particles.push(new Circle(x, y, 5, 'white', { x: Math.cos(radian * i), y: Math.sin(radian * i) }));
     }
 }
 
 function animate() {
     requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
 
+    // c.globalCompositeOperation = 'lighter';
     particles.forEach((particle, i) => {
         if (particle.tTL < 0) {
             particles.splice(i, 1);
